fe027322896797728ee21b5cb074d867e8a6d49f
rafailsohbatov <sohbatov.rafail@gmail.com>
2023-07-20 13:12:02 +0400

    Added FileUtilities

--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -1,17 +1,43 @@
 package main;
 
 
+import util.FileUtilities;
+
 import java.util.Scanner;
 
 public class MainClass {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Scanner scan = new Scanner(System.in);
         System.out.println("admin or user ?");
-        switch (scan.next()){
+        switch (scan.next()) {
             case "admin":
                 System.out.println("1.Luget yarat\n2.Lugete soz elave et");
+                switch (scan.nextInt()) {
+                    case 1:
+                        System.out.print("Enter New Dictionary Name: ");
+                        String newFileName = scan.next();
+                        FileUtilities.createFile(newFileName);
+                        break;
+                    case 2:
+                        System.out.print("Enter Word 1: ");
+                        String word1 = scan.next();
+                        System.out.print("Enter Word 2: ");
+                        String word2 = scan.next();
+                        System.out.print("Enter File Name: ");
+                        String fileName = scan.next();
+                        FileUtilities.writeWordToFile(word1,word2,fileName);
+                        break;
+                    default:
+                        System.out.println("Bu secim yoxdur");
+                }
                 break;
             case "user":
+                System.out.print("Enter Dictionary Name: ");
+                String fileName = scan.next();
+                System.out.print("Enter Word: ");
+                String word = scan.next();
+                String result =  FileUtilities.findWord(word,fileName);
+                System.out.println("Translated Word: " + result);
                 break;
             default:
                 System.out.println("Bu istifadeci yoxdur");
---
+++ src/main/java/util/FileUtilities.java
@@ -0,0 +1,35 @@
+package util;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+public class FileUtilities {
+    public static void writeWordToFile(String word1, String word2, String fileName) throws Exception {
+        BufferedWriter bw = new BufferedWriter(createFile(fileName));
+        bw.write(word1 + "-" + word2);
+        bw.newLine();
+        bw.close();
+    }
+
+    public static FileWriter createFile(String fileName) throws Exception {
+        FileWriter fw = new FileWriter(fileName,true);
+        return fw;
+    }
+
+    public static String findWord(String word, String fileName) throws Exception {
+        BufferedReader bw = new BufferedReader(new FileReader(fileName));
+        Map<String, String> map = new HashMap<>();
+        while (bw.ready()) {
+            String[] arr = bw.readLine().split("-");
+            map.put(arr[0], arr[1]);
+            map.put(arr[1], arr[0]);
+        }
+        String result = map.get(word);
+        map.clear();
+        return result;
+    }
+}
